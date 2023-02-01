1043a88e54cb2204a7341ef6987c7876dbc00bfe
Alim1899 <alimakhchanogli@gmail.com>
2023-02-01 13:43:11 +0400

    Completed tip % calculation section

--- script.js
+++ script.js
@@ -14,18 +14,24 @@ const reset = document.querySelector('.reset');
 const customInput = document.querySelector('.customInput');
 
 
-let percent ;
-
+let percent = 0 ;
     opts.addEventListener('click',function(e){
         e.preventDefault()
+        
        if(e.target.value){
+
+        if(parseInt(e.target.textContent)===percent){
+        options.forEach(el=>el.style.backgroundColor = '');
+        percent=0;
+        e.target.backgroundColor = 'red';
+       }else
          if (e.target.value=='tip') {
-            
             customInput.style.display = 'none';
             opt6.style.display = 'block';
          options.forEach(el=>el.style.backgroundColor = '');
        e.target.style.backgroundColor = 'darkCyan';
        percent = parseInt(e.target.textContent);
+       
     }
      if(e.target.value!='tip'){
         options.forEach(el=>el.style.backgroundColor = '');
@@ -35,13 +41,12 @@ let percent ;
         customInput.style.display = 'block';
         customInput.addEventListener('input',function(t){
             e.preventDefault();
-            console.log(t.target.value, "shiitt");
-            percent = t.target.value;
-            
+            if(t.target.value>0){
+                percent = parseInt(t.target.value);
+            }else percent = 0; 
         })
     }
-    
-}
+};
        }     
    )
 
@@ -54,8 +59,9 @@ reset.addEventListener('click',function(){
    location.reload();
 })
 
+
 const calculate = function(){
-    const bill = parseInt();
+    const bill = parseInt(document.querySelector('.bill'));
     const people = parseInt(document.querySelector('.people').value);
     const tip = bill/100*percent;
     const amount = (bill +tip)/people;
@@ -64,3 +70,11 @@ const calculate = function(){
     document.querySelector('.amountSplit').textContent=tip/people;
     document.querySelector('.totalSplit').textContent=amount
 }
+
+document.querySelector('.people').addEventListener('input',function(){
+   if(percent>0) console.log(percent);
+   if(percent==0)console.log('shit');
+    const bill = document.querySelector('.bill').value;
+    if(!bill) return;
+    if(bill>0) console.log('1');
+})
